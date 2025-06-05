2c9e5291c672134b391e5f574326c276b8545dcb
Camila Alvarez <cam.alvarez.i@gmail.com>
2025-06-05 19:26:19 -0400

    fix(service): Fix parse results
    Signed-off-by: Camila Alvarez <cam.alvarez.i@gmail.com>

--- backend/app/app/services/runner_service.py
+++ backend/app/app/services/runner_service.py
@@ -1,6 +1,7 @@
 from app.schemas.tests import RunnerTestsResults
 from app.services.kcidb_services import KCIDBTestSubmission
 from app.utils.test_parser import generate_test_id, get_test_path
+from datetime import timezone, datetime
 
 
 async def parse_results2kcidb(tests_results: RunnerTestsResults) -> list[KCIDBTestSubmission]:
@@ -8,6 +9,7 @@ async def parse_results2kcidb(tests_results: RunnerTestsResults) -> list[KCIDBTe
     for test in tests_results.tests:
         path = get_test_path(test.test_collection, test)
         test_id = generate_test_id(tests_results.test_uid, test.test_collection, test.test_name)
-        parsed_results.append(KCIDBTestSubmission(path, test.result, test.logs, test_id, tests_results.build_id, tests_results.runtime, tests_results.device))
+        parsed_results.append(KCIDBTestSubmission(path, test.result, test.logs, test_id, tests_results.build_id, datetime.now(timezone.utc),
+                                                   tests_results.runtime, tests_results.device))
     
     return parsed_results
